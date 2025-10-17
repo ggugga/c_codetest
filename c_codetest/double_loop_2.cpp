697c8062c4844f569a73ba4e2684cedbc0ebccb4
Gugagyeong <rkrud616@naver.com>
2025-10-17 12:52:37 +0900

    중첩된 반복문 연습문제 2

---
+++ c_codetest/double_loop_2.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+    int j = 0;
+    int count = 0;
+
+    while (j < 5)
+    {
+        i = 0;
+        while (i < 4-j)
+        {
+            printf(" ");
+            ++i;
+        }
+        count = 0;
+        while (count < (2 * j + 1))
+        {
+            printf("*");
+            count++;
+        }
+        printf("\n");
+        ++j;
+    }
+
+    j = 0;
+    int k = 3;
+    while (j < 5)
+    {
+        i = 0;
+        while (i <= j)
+        {
+            printf(" ");
+            ++i;
+        }
+        count = 0;
+        while (count < (2 * k + 1))
+        {
+            printf("*");
+            count++;
+        }
+        --k;
+        printf("\n");
+        ++j;
+        } 
+        
+        
+
+    return 0;
+}
